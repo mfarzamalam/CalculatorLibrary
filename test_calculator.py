c338541d43af9feda39bb081116a11f0930ec404
mfarzamalam <mfarzamalam@gmail.com>
2021-07-20 22:18:41 +0500

    added some whitespaces

--- test_calculator.py
+++ test_calculator.py
@@ -1,9 +1,11 @@
 import calculator
 
 class TestCalculator:
-    
+
+
     def test_add(self):
-        assert 4 == calculator.add(2,2)
+        assert 4 == calculator.add(2, 2)
+
 
     def test_subtract(self):
-        assert 2 == calculator.subtract(4,2)
+        assert 2 == calculator.subtract(4, 2)
