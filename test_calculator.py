09e63240525ba865dca78759b286a325a9a6a7b4
mfarzamalam <mfarzamalam@gmail.com>
2021-07-20 22:42:43 +0500

    added two more function and test

--- test_calculator.py
+++ test_calculator.py
@@ -8,3 +8,9 @@ class TestCalculator:
 
     def test_subtract(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multiply(self):
+        assert 4 == calculator.multiply(2, 2)
+
+    def test_divide(self):
+        assert 2 == calculator.divide(4, 2)
